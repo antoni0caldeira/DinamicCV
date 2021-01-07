23d07bb4635b6ebe7c61eb560078c6c0780aa382
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-01-07 15:27:17 +0000

    Migration

---
+++ DinamicCV/Data/Migrations/20210107152607_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DinamicCV.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkData",
+                columns: table => new
+                {
+                    WorkDataId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Employer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InitialDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FinalDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    JobTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JobDescription = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkData", x => x.WorkDataId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkData");
+        }
+    }
+}
