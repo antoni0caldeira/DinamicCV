e3a1e859f6456aaa8ba38f73b102bc8e4772e001
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-01-07 14:08:50 +0000

    Criação de Contolador

---
+++ DinamicCV/Controllers/WorkDataController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinamicCV.Controllers
+{
+    public class WorkDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
