41b69bf12efcf4db6cce93e23b0626a1662d1120
X ONE <antoniocaldeira@yahoo.com>
2021-02-02 21:47:08 +0000

    adição de classe greatings método saudacao()

---
+++ DinamicCV/Models/Greatings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinamicCV.Models
+{
+    public class Greatings
+    {
+        public static String Saudacao()
+        {
+
+            int hora = DateTime.Now.Hour;
+            string mensagem;
+            if (hora >= 7 && hora < 12)
+            {
+                mensagem = "Bom dia ";
+            }
+            else if (hora >= 12 && hora < 20)
+            {
+                mensagem = "Boa tarde ";
+            }
+            else
+            {
+                mensagem = "Boa noite ";
+            }
+
+            return mensagem;
+        }
+    }
+
+
+
+
+}
