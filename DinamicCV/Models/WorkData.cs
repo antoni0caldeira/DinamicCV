752ab2ef9376c51008c09778c778f1af0ffb8521
X ONE <antoniocaldeira@yahoo.com>
2021-02-02 21:09:16 +0000

    Adicição de alguns dysplay ao controlador

--- DinamicCV/Models/WorkData.cs
+++ DinamicCV/Models/WorkData.cs
@@ -7,22 +7,23 @@ namespace DinamicCV.Models
     {
         public int WorkDataId { get; set; }
         //[Required(ErrorMessage = "O nome do Empregador é obrigatório")]
+        [Display(Name = "Empregador")]
         public String Employer { get; set; }
         //[Required(ErrorMessage = "A data Inicial é para preencher")]
         //[DataType(DataType.Date)]
 
-
+        [Display(Name = "Data Inicial")]
         public DateTime InitialDate { get; set; }
         //[Required(ErrorMessage = "A data Final também")]
         //[DataType(DataType.Date)]
 
-
+        [Display(Name = "Data Final")]
         public DateTime FinalDate { get; set; }
         //[Required(ErrorMessage = "Campo de preenchimento obrigatório")]
-
+        [Display(Name = "Profissão")]
         public String JobTitle { get; set; }
         //[Required(ErrorMessage = "Campo de preenchimento obrigatório")]
-
+        [Display(Name = "Descrição da Profissão")]
         public String JobDescription { get; set; }
     }
 }
